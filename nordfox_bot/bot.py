ec531acb5a57c3935a4b93c6f2989f795a0bf152
Maxim Zelensky <dragoonov58@icloud.com>
2024-01-07 12:11:22 +0300

    Поправил стартовый текст

--- nordfox_bot/bot.py
+++ nordfox_bot/bot.py
@@ -5,6 +5,6 @@ bot = telebot.TeleBot(settings.API_KEY)
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}\nКакой договор вам нужен?')
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}\nВыберите, какие документы вы хотите получить:')
 
 bot.polling(none_stop=True)
