171e7fd5e6622c8fbd262492ca2cebd8941e33e8
Maxim Zelensky <dragoonov58@icloud.com>
2024-01-15 14:10:42 +0300

    Продолжаем кодить

---
+++ lesson2/ask_user.py
@@ -0,0 +1,5 @@
+def ask_user():
+    a = input('Как дела?')
+    while a != 'Хорошо':
+        continue
+ask_user
---
+++ lesson2/list.py
@@ -0,0 +1,4 @@
+line = input('Введите строковые данные')
+list_line = list(line)
+for i in list_line:
+    print(i)
