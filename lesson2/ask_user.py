688d86a2a7be821d9ba05aa09c0fd718dae38431
Maxim Zelensky <dragoonov58@icloud.com>
2024-01-19 22:14:58 +0300

    Добавил обработку исключения

--- lesson2/ask_user.py
+++ lesson2/ask_user.py
@@ -4,12 +4,14 @@ def ask_user():
         'Что делаешь?' : 'Программироваю',
         'Как погода?' : 'Заебись'
     }
-    while True:
-        question = input('Введите свой вопрос   ')
-        if question in faq:
-            print(faq.get(question))
-            break
-        else:
-            print('Давайте попробуем еще раз')
-
+    try:
+        while True:
+            question = input('Введите свой вопрос   ')
+            if question in faq:
+                print(faq.get(question))
+                break
+            else:
+                print('Давайте попробуем еще раз')
+    except KeyboardInterrupt:
+        print("\nПрограмма прервана пользователем. До свидания!")
 ask_user()
