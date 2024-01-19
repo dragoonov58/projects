c6ada1434312ef578271e1e69640f838c9b0903f
Maxim Zelensky <dragoonov58@icloud.com>
2024-01-19 21:34:23 +0300

    Прошел итерацию по словарю

--- lesson2/ask_user.py
+++ lesson2/ask_user.py
@@ -1,5 +1,15 @@
 def ask_user():
-    a = input('Как дела?')
-    while a != 'Хорошо':
-        continue
-ask_user
+    faq = {
+        'Как дела?' : 'Хорошо',
+        'Что делаешь?' : 'Программироваю',
+        'Как погода?' : 'Заебись'
+    }
+    while True:
+        question = input('Введите свой вопрос   ')
+        if question in faq:
+            print(faq.get(question))
+            break
+        else:
+            print('Давайте попробуем еще раз')
+
+ask_user()
